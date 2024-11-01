35ae281d02ee832258e3fa51398863750728b59b
Daniel <daniel.e.san.1999@gmail.com>
2024-11-01 19:30:34 -0400

    Added a simple score system to the game

--- main.js
+++ main.js
@@ -1,7 +1,42 @@
-let buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
+const points = document.querySelector('#points');
+const winner = document.querySelector('#winner');
+const gamePoints = [0, 0];
+updateGamePoints();
+
+function updateGamePoints () {
+    points.innerHTML = `
+    The current puntuation is: <br>
+    -Player: ${gamePoints[0]} <br>
+    -CPU: ${gamePoints[1]}`;
+}
+
+function updatePoints (playerPoint) {
+    winner.textContent = '';
+    switch (playerPoint) {
+        case 'player':
+            gamePoints[0]++;
+            updateGamePoints();
+            if (gamePoints[0] === 5) {
+                winner.textContent = 'The Player WINS!';
+                gamePoints[0]= 0;
+                gamePoints[1]= 0;
+            }
+            break;
+        case 'cpu':
+            gamePoints[1]++;
+            updateGamePoints();
+            if (gamePoints[1] === 5) {
+                winner.textContent = 'The CPU WINS!';
+                gamePoints[0]= 0;
+                gamePoints[1]= 0;
+            }
+            break;
+    }
+}
 
 function getComputerChoice() {
-    let num= Math.floor(Math.random()*3);
+    const num= Math.floor(Math.random()*3);
     switch (num) {
         case 0:
             return 'ROCK';
@@ -22,22 +57,28 @@ function playRound(playerChoise, computerChoise) {
             case 'ROCK':
                 switch (computerChoise) {
                     case 'PAPER':
+                        updatePoints('cpu');
                         return 'You Lose! Paper beats Rock'
                     case 'SCISSORS':
+                        updatePoints('player');
                         return 'You Win! Rock beats Scissors'
                 }
             case 'PAPER':
                 switch (computerChoise) {
                     case 'ROCK':
+                        updatePoints('player');
                         return 'You Win! Paper beats Rock'
                     case 'SCISSORS':
+                        updatePoints('cpu');
                         return 'You Lose! Scissors beats Paper'
                 }
             case 'SCISSORS':
                 switch (computerChoise) {
                     case 'ROCK':
+                        updatePoints('cpu');
                         return 'You Lose! Rock beats Scissors'
                     case 'PAPER':
+                        updatePoints('player');
                         return 'You Win! Scissors beats Paper'
                 }
             default:
